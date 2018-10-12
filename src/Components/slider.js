46882185dbd515c0327d5565b980eef48cff55f5
Olamilekan Odukoya <olarpringo@gmail.com>
2018-10-12 11:37:36 +0100

    fixed slider

--- src/Components/slider.js
+++ src/Components/slider.js
@@ -12,27 +12,21 @@ class AutoPlay extends Component {
 				description:
 				'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.',
 				button: 'Read More',
-				image: 'https://i.imgur.com/ZXBtVw7.jpg',
-				user: 'Luan Gjokaj',
-				userProfile: 'https://i.imgur.com/JSW6mEk.png'
+				image: 'http://www.masteracct.com/assets/img/sliders/2.jpg'
 			},
 			{
 				title: 'Tortor Dapibus Commodo Aenean Quam',
 				description:
 				'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.',
 				button: 'Discover',
-				image: 'https://i.imgur.com/DCdBXcq.jpg',
-				user: 'Erich Behrens',
-				userProfile: 'https://i.imgur.com/0Clfnu7.png'
+				image: 'http://www.masteracct.com/assets/img/sliders/5.jpg',
 			},
 			{
 				title: 'Phasellus volutpat metus',
 				description:
 				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.',
 				button: 'Buy now',
-				image: 'https://i.imgur.com/DvmN8Hx.jpg',
-				user: 'Bruno Vizovskyy',
-				userProfile: 'https://i.imgur.com/4KeKvtH.png'
+				image: 'http://www.masteracct.com/assets/img/sliders/Slider_Payroll_Services.jpg'
 			}
 		]
 	}
@@ -44,15 +38,15 @@ class AutoPlay extends Component {
 			duration: 2000
 		}
 		return (
-			<div>
-				<Slider className="center slider-wrapper previousButton slider nextButton next animateIn animateOut" {...settings}>
+			<div className="track slider-wrapper slider">
+				<Slider className="center previousButton nextButton next animateIn animateOut" {...settings}>
 					{this.state.content.map((item, index) => (
 						<div
 							key={index}
 							className="slider-content"
 							style={{ background: `url('${item.image}') no-repeat center center` }}
 						>
-							<div className="container">
+							<div className="container contents">
 								<h1>{item.title}</h1>
 								<p>{item.description}</p>
 								<button className="btn-large red darken-3">{item.button}</button>
